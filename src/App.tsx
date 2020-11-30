814b75d80086c174312abcefc197ec97851043f9
Ian Elizondo <ianelicha@gmail.com>
2020-11-29 18:14:57 -0600

    🔧Updated libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import Loading from './screens/loading';
+import SignIn from './screens/signIn';
+import {
+  ApolloProvider,
+  HttpLink,
+  split,
+  ApolloLink,
+  ApolloClient,
+  InMemoryCache,
+} from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { graphQlServer } from './secretKey';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import Main from './screens/main';
 
 interface AppProps {}
 
+const wsLink = new WebSocketLink({
+  uri: graphQlServer.ws,
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: graphQlServer.url,
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  (wsLink as unknown) as ApolloLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  credentials: 'include',
+  link: splitLink,
+});
+
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
   );
 }
 
